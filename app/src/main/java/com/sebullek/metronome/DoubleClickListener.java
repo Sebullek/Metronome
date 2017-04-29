b85b5786ace2d6a423c8e8b2cb4ca63650f7f3f6
Sebullek <seba.mich93@gmail.com>
2017-04-29 13:32:13 +0200

    Added EditSpinner Class

---
+++ app/src/main/java/com/sebullek/metronome/DoubleClickListener.java
@@ -0,0 +1,68 @@
+package com.sebullek.metronome;
+
+import android.os.Handler;
+import android.view.View;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * Created by Sebullek on 17.04.2017.
+ */
+
+public abstract class DoubleClickListener implements View.OnClickListener/*, View.OnFocusChangeListener */{
+
+
+    private Timer timer = null;  //at class level;
+    private int DELAY   = 400;
+
+    private static final long DOUBLE_CLICK_TIME_DELTA = 300;//milliseconds
+
+    static long lastClickTime = 0;
+
+    @Override
+    public void onClick(View v) {
+        long clickTime = System.currentTimeMillis();
+        if (clickTime - lastClickTime < DOUBLE_CLICK_TIME_DELTA){
+            processDoubleClickEvent(v);
+        } else {
+            processSingleClickEvent(v);
+        }
+        lastClickTime = clickTime;
+    }
+
+    public void processSingleClickEvent(final View v){
+
+        final Handler handler=new Handler();
+        final Runnable mRunnable=new Runnable(){
+            public void run(){
+                onSingleClick(v); //Do what ever u want on single click
+
+            }
+        };
+
+        TimerTask timertask = new TimerTask(){
+            @Override
+            public void run(){
+                handler.post(mRunnable);
+            }
+        };
+        timer=new Timer();
+        timer.schedule(timertask,DELAY);
+
+    }
+
+
+    public void processDoubleClickEvent(View v){
+        if(timer!=null)
+        {
+            timer.cancel(); //Cancels Running Tasks or Waiting Tasks.
+            timer.purge();  //Frees Memory by erasing cancelled Tasks.
+        }
+        onDoubleClick(v);//Do what ever u want on Double Click
+    }
+
+    public abstract void onSingleClick(View v);
+
+    public abstract void onDoubleClick(View v);
+}
